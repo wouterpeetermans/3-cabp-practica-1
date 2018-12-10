f8f721a951b459ce3646c710a1c2608e4b28717d
Wouter Peetermans <wouter@wouterpeetermans.com>
2018-12-10 18:55:17 +0100

    report made

--- fileconsumer/src/main.cpp
+++ fileconsumer/src/main.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+// little function to do some processor work
 int cpuAddOneToArray(char* array, int size){
   for (int i = 0; i < size; i++){
     array[i]++;
@@ -15,40 +16,35 @@ int cpuAddOneToArray(char* array, int size){
 int main(int argc, char* argv[]){
   char * memblock;
   streamsize size;
-  string filename = "testing.bin";
+  string filename = "testing.bin";    //default filename 
   if ( argc > 1 ){
     filename = argv[1];
   }
 
-  auto start = chrono::high_resolution_clock::now();
-  ifstream file (filename,ios::in | ios::ate | ios::binary);
+  auto start = chrono::high_resolution_clock::now();  //take starttime
+  ifstream file (filename,ios::in | ios::ate | ios::binary); // file reading sequence
   if (file.is_open()){
     size = file.tellg();
     file.seekg (0, ios::beg);
     memblock = new char[size];
     file.read(memblock, size);
     file.close();
-    //cout << "the size is: " << size << endl;
   }
-  auto finish = chrono::high_resolution_clock::now();
+  auto finish = chrono::high_resolution_clock::now(); //take endtime
+  //print the time it took to the console
   cout << chrono::duration_cast<chrono::nanoseconds>(finish - start).count() << "ns\n";
   start = chrono::high_resolution_clock::now();
-  cpuAddOneToArray(memblock,size);
+  cpuAddOneToArray(memblock,size); //room heating :) 
   finish = chrono::high_resolution_clock::now();
-
   cout << chrono::duration_cast<chrono::nanoseconds>(finish - start).count() << "ns\n";
-
   start = chrono::high_resolution_clock::now();
-
-  ofstream wfile (filename,ios::out | ios::trunc | ios::binary);
+  ofstream wfile (filename,ios::out | ios::trunc | ios::binary); //file writing sequence
   if(wfile.is_open()){
     wfile.write(memblock, size);
     wfile.close();
   }
   finish = chrono::high_resolution_clock::now();
-
   cout << chrono::duration_cast<chrono::nanoseconds>(finish - start).count() << "ns\n";
-
   return 0;
 }
 
--- filecreator/src/main.cpp
+++ filecreator/src/main.cpp
@@ -7,10 +7,10 @@ using namespace std;
 int mkFile(int size,string filename){
   char* memblock;
   memblock = new char[size];
-  for( int i = 0; i < size; i++){
+  for( int i = 0; i < size; i++){ //fill up some space with random data
     memblock[i] = rand()%255; 
   }
-  ofstream file (filename, ios::out | ios::trunc | ios::binary);
+  ofstream file (filename, ios::out | ios::trunc | ios::binary); //file writing sequence
   if (file.is_open()){
     file.write(memblock, size);
     file.close();
@@ -21,16 +21,16 @@ int mkFile(int size,string filename){
 
 
 int main(int argc, char* argv[]){
-  string filename = "testing.bin";
+  string filename = "testing.bin";//default file name and size if none are given
   int fileSize = 1024;
-  if ( argc > 1 ){
+  if ( argc > 1 ){      //first argument becomes filename
     filename = argv[1];
   }
-  if ( argc > 2 ){
-    fileSize = stoi (argv[2],nullptr);
+  if ( argc > 2 ){      //second argument becomes filesize 
+    fileSize = stoi (argv[2],nullptr); // string to int function
   }
   
-  mkFile(fileSize,filename);
+  mkFile(fileSize,filename);//see defenition above
   return 0;
 }
 
